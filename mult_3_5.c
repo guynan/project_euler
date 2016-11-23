cb47e97ba2f22f317bc0d0625c1b7320a3e1c588
Guy Nankivell <guy.nankivell@solnet.com>
2016-11-23 14:58:14 +1300

    First solution

---
+++ mult_3_5.c
@@ -0,0 +1,33 @@
+/* Sum of multiples of 3 and 5 below 1000 
+ * Project Euler: 1
+ * Answer: 233168 */
+
+/* Includes */
+#include <stdio.h>
+
+/* Definitions */
+#define MAX     1000
+
+/* Prototypes */
+int main();
+int check(int i);
+
+int main()
+{
+        register int i; int sum = 0;
+
+        for(i = 0; i< MAX; i++){
+                sum += (check(i)) ? i : 0;
+        }
+
+        printf("%d\n", sum);
+}
+
+int check(int i)
+{
+        int check;
+
+        check = (i % 3 == 0 || i % 5 == 0) ? 1 : 0;
+
+        return check;
+}

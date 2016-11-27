8f58c4b69f7642ad9cafe0e58a29f0f24fbf856d
Guy Nankivell <guy.nankivell@gmail.com>
2016-11-27 23:19:47 +1300

    Done but messy

---
+++ largest_palindrome.c
@@ -0,0 +1,78 @@
+/* A palindromic number reads the same both ways.
+ * The largest palindrome made from the product
+ * of two 2-digit numbers is 9009 = 91 × 99.
+ *
+ * Find the largest palindrome made from
+ * the product of two 3-digit numbers.
+ * Project Euler: 4 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX     999
+
+/* Prototypes */
+int main();
+int is_palindrome(int i);
+int len(int i);
+
+int main()
+{
+        register int test; int longest;
+
+        /* Fuck this waterfall, like really */
+        for(int i = 0; i < MAX; i++){
+                for(int a = 0; a < MAX; a++){
+                        test = i * a;
+                        if(is_palindrome(test)){
+                                if(test > longest){
+                                        longest = test;
+                                }
+                        }
+                }
+        }
+
+        printf("%d\n", longest);
+
+        return 0;
+
+}
+
+/* Checks if it is a palindrome
+ * through string manipulation */
+int is_palindrome(int i)
+{
+        int length = len(i);
+        char strcpy[length]; char check[length];
+        
+        sprintf(strcpy, "%d", i);
+
+        length--;
+
+        for(int a = 0; length >= 0; length--, a++){
+                check[length] = strcpy[a];
+        }
+
+        for(int l = 0; check[l] != '\0' ; l++){
+                if(check[l] != strcpy[l]){
+                        return 0;
+                }
+        }
+
+        return 1;
+
+}
+
+
+/* lol will return 6 always */
+int len(int i)
+{
+        int a = 0; char strcpy[6];
+        
+        sprintf(strcpy, "%d", i);
+        for(; strcpy[a] != '\0'; a++);
+
+        return a;
+}
+

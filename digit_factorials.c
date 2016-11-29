e73ddb793c46400a759066322448a02a1299c9af
Guy Nankivell <guy.nankivell@solnet.com>
2016-11-29 16:32:02 +1300

    Additional work

--- digit_factorials.c
+++ digit_factorials.c
@@ -7,41 +7,52 @@
  * not sums they are not included.
  * Project Euler: 34 */
 
+/* Includes */
 #include <stdio.h>
 
-#define MAX     9999999999
+/* Definitions */
+#define MAX     2540160  
 #define LEN     21 /* Will be << n! */
 long long factorials[LEN];
-int init_factorials();
 
+/* Prototypes */
+int is_digit_factorial(long long i);
+int init_factorials();
+int ctoint(char c);
+int len(long long i);
 int main();
+
 
 int main()
 {
-        int count; 
+        int count = 0;
         init_factorials();
 
-        for(int i = 0; i < 56; i++){
-                printf("%lli\n", factorials[i]);
-        }
-        for(int c = 0; ; c++){
+        for(long long c = 10; ; c++){
+
                 if(is_digit_factorial(c)){
+                        printf("%lli\n", c);
                         count++;
                 }
 
-                if(
+//                if(factorials[c] > MAX){
+ //                       break;
+  //              }
+        }
+
+        printf("%d\n", count);
 
         return 0;
 
 }
 
-long long 
 
 int init_factorials()
 {
         factorials[0] = 1; factorials[1] = 1;
         factorials[2] = 2;
         long long last = 2;
+
         for(long long i = 3; i < LEN; i++){
                 factorials[i] = last * i;
                 last = factorials[i];
@@ -50,23 +61,26 @@ int init_factorials()
         return 0;
 }
 
-int is_digit_factorial(int i)
+int is_digit_factorial(long long i)
 {
-        char strcpy[32];
+        char strcpy[32]; long sum = 0;
+        int indexnum;
 
-        sprintf(strcpy, "%d", i);
+        sprintf(strcpy, "%lli", i);
 
-        for 
+        for(int c = 0; c < len(i); c++){
+                indexnum = ctoint(strcpy[c]);
+                sum += factorials[indexnum];
+        }
 
-
-
+        return(sum == i);  
 }
 
-int len(int i)
+int len(long long i)
 {
-        char strcpy[32]; int c = 0
+        char strcpy[32]; int c = 0;
 
-        sprintf(strcpy, "%li", i);
+        sprintf(strcpy, "%lli", i);
 
         for(; strcpy[c] != '\0'; c++);
 
@@ -75,5 +89,5 @@ int len(int i)
 
 int ctoint(char c)
 {
-        return char - 48;
+        return c - 48;
 }

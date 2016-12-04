442c37e8b0128311f63fe980d7d7c8eced502d63
Guy Nankivell <guy.nankivell@gmail.com>
2016-12-04 23:26:37 +1300

    ^^ ditto

--- summation_primes.c
+++ summation_primes.c
@@ -1,10 +1,13 @@
 /* Finding the summation of primes < 2mil
  * This takes fucking ages
  *
- * Project Euler: 7 */
+ * Answer: 142913828922 
+ *
+ * Project Euler: 10 */
 
 /* Includes */
 #include <stdio.h>
+#include <math.h>
 
 /* Definitions */
 #define MAX 2000000
@@ -15,9 +18,9 @@ int main();
 
 int main()
 {
-        long long unsigned int primesum = 0;
+        long long unsigned int primesum = 2;
 
-        for(unsigned int i = 0; i < MAX; i++){
+        for(unsigned int i = 3; i < MAX; i++){
                 if (isprime(i)){
                         primesum += i;
                 }
@@ -31,9 +34,10 @@ int main()
 int isprime(unsigned int s)
 {
         int isprime = 0; /* False */
+        int top = (int) round(sqrt(s))+1;
 
-        for(int i = 2; i < s+1; i++){
-                if(i == s){
+        for(int i = 2; i < top +1 ; i++){
+                if(i == top){
                         isprime = 1; /* True */
                         break; 
                 }

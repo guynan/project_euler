09d83e6a11ce4614575e71024173b01475314337
Guy Nankivell <guy.nankivell@solnet.co.nz>
2016-12-08 14:11:32 +1300

    Finished; may tidy later

--- 1000-digit-fib.c
+++ 1000-digit-fib.c
@@ -2,43 +2,66 @@
  * sequence that will have over 1000 digits?
  *
  * Project Euler: 25
- * Answer:  */
+ * Answer: 4782 */
 
 /* Includes */
 #include <stdio.h>
 #include <gmp.h>
 
+/* FreeBSD : ln lib from /usr/local/lib to /usr/lib 
+ * && ln gmp.h from /usr/local/include to /usr/include */
+
 /* Definitions */
-#define MAX 10
+#define MAX 1000
+mpz_t first;
+mpz_t second;
+mpz_t term;
 
 /* Prototypes */
 int main();
+int len(mpz_t i);
 
 int main()
 {
-        int index;
+        /* So we can set our mpz_t to 1 */
+        unsigned long int one = 1;
+        
 
-        int first = 1; int second = 1; int term;
+        mpz_init(term); mpz_init(first); mpz_init(second); 
+        /* Set these to one */
+        mpz_set_ui(first, one); mpz_set_ui(second, one);
+        int i = 3;
 
-        /* Create array of Fibonacci values < MAX */
-        for(int i = 2; i < MAX; i++){
+        for( ; ; i++){
 
-                term = first + second;
-                printf("%d\n", term);
+                mpz_add(term, first, second);
 
-                first = second;
-                second = term;
-/*
-                if (len(term)> MAX){
+                /* Set first = second */
+                mpz_set(first, second);
+                mpz_set(second, term);
+
+                if (len(term) ==  MAX){
                         break;
                 }
-*/
 
         }
 
-//        printf("%d\n", index);
+        gmp_printf("%Zd\n\n", term);
+        printf("%d\n", i);
 
         return 0;
 
 }
 
+int len(mpz_t i)
+{
+        int a = 0; char strngcpy[1001];
+
+        /* Have to cast to string to 
+         * get length */
+        mpz_get_str(strngcpy, 10, i);
+
+        for( ; strngcpy[a] != '\0'; a++);
+
+        return a;
+}

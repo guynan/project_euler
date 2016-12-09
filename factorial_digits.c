7c8f0c998db6bed3a98597f54efeed6d5b63fa32
Guy Nankivell <guy.nankivell@solnet.co.nz>
2016-12-10 12:45:27 +1300

    I did this

--- factorial_digits.c
+++ factorial_digits.c
@@ -1,22 +1,35 @@
 /* Find the sum of all the numbers which 
  * are equal to the sum of the factorial
  * of their digits. 
+ *
+ * Answer: 40730
  * Project Euler: 34 */
 
 /* Includes */
 #include <stdio.h>
 
 /* Definitions */
-#define MAX     150
+
+/* Find way of not setting arbitrary num */
+#define MAX     1600000 
 
 /* Prototypes */
 int main();
+int ctoint(char c);
 long factorial(long f);
+int check_digit_fact(long i);
+
 
 int main()
 {
-        for(int i = 0; ){ /* Finished here */
-        printf("%li", factorial(4));
+        long sum = 0;
+
+        for(long i = 5; i < MAX; i++ ){ 
+                sum += (check_digit_fact(i)) ? i : 0;
+        }
+
+        printf("%li\n", sum);
+
 }
 
 
@@ -30,3 +43,27 @@ long factorial(long f)
 
         return factorial;
 }
+
+int check_digit_fact(long i)
+{
+        char string[16];
+
+        /* Cast num to string */
+        sprintf(string, "%li", i);
+        
+        int sum = 0; int digit;
+
+        for(int c = 0; string[c] != '\0'; c++){
+                digit = ctoint(string[c]);
+                sum += factorial(digit); 
+        }
+        
+        return (sum == i);        
+}
+
+int ctoint(char c)
+{
+        int x = (int) c - 48;
+        return x;
+}
+

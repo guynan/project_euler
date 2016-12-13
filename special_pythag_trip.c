d1041b8c9ad628948939aeb243c4f86b9c33f1e8
Guy Nankivell <guy.nankivell@solnet.co.nz>
2016-12-14 08:56:15 +1300

    Finally got around to writing this shitty loop

--- special_pythag_trip.c
+++ special_pythag_trip.c
@@ -5,15 +5,55 @@
  * There exists exactly one Pythagorean triplet 
  * for which a + b + c = 1000.
  * Find the product abc.
+ * 
+ * Answer: 31875000
  * Project Euler: 9 */
 
 #include <stdio.h>
+#include <math.h>
 
 #define MAX     1000
 
 /* Prototypes */
 int main();
+int ispythag(int a, int b, int c);
+int sumcheck(int a, int b, int c);
 
 int main()
 {
+        int product = 0;
+
+        for(int a = 0; a <= MAX; a++){
+                for(int b = 0; b <= MAX; b++){
+                        for(int c = 0; c <= MAX; c++){
+                                if(sumcheck(a, b, c) && ispythag(a, b, c)){
+                                        product = a * b * c;
+                                        break; 
+                                }
+                        }
+
+                        if(product != 0){
+                                break;
+                        }
+                }
+
+                if(product != 0){
+                        break;
+                }
+        }
+       
+        printf("%d\n", product); 
+
+        return 0;
 }
+
+int ispythag(int a, int b, int c)
+{
+        return (pow(a, 2) + pow(b, 2) == pow(c, 2));
+}
+
+int sumcheck(int a, int b, int c)
+{
+        return (a + b + c == MAX);
+}
+                

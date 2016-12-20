9afe359d1f167946e708b7e6dd7f05175d666f2a
Guy Nankivell <guy.nankivell@solnet.co.nz>
2016-12-20 13:49:57 +1300

    Working on a few different ways of getting this to work

--- progress/distinct_powers.c
+++ progress/distinct_powers.c
@@ -6,107 +6,140 @@
 
 /* Includes */
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <gmp.h>
+
 
 /* Definitions */
-#define MAX_A   1000
-#define MAX_B   1000
-#define MAXLEN  1000000
-unsigned long long int powers[MAXLEN];
+#define MAX_A   100
+#define MAX_B   100
+#define MAXLEN  10000
+char powers[MAXLEN];
 
 /* Prototypes */
 long len(unsigned long int list[]);
-int inlist(long long int i, int count);
-unsigned long long power(int a, int b);
+append(mpz_t i, int count);
+int inlist(mpz_t i);
+void bubble_sort(long list[], long n);
 int main();
+
 
 int main()
 {
-        int temp = 0;
         int count = 0;
-        unsigned long long int i = 0;
+        unsigned long long int a = 2;
+        unsigned long long int b = 2;
 
-        int a; int b;
+        /* As we have a theoretical maximum 
+         * of 100**100 we need to initialise
+         * mpz_t ints so we don't overflow */
+
+        mpz_t i;
+        mpz_init(i);
+
+        /*
+        for (int f = 0; f < MAXLEN; f++) {
+                    mpz_init2(powers[f], 1024);
+        }
+
+	extern mpz_t *powers;
+	powers = malloc(MAXLEN * sizeof(mpz_t));
+	mpz_array_init(*powers, MAXLEN, 1024);
+        */
+
+        /* HACK: Instead of array of mpz_t, convert all of them 
+         * to char and then mpz_set_str to conv them back to mpz_t
+         * and compare the ints */
 
         for(a = 2; a <= MAX_A; a++){
                 for(b = 2; b <= MAX_B; b++){
-                        i = pow(a,b);
-                        temp++;
+
+			/* Set i = a ** b */
+                        mpz_ui_pow_ui(i, a, b);
                         
                         /* Insert in list of not already */
-                        if (!inlist(i, count)){
-                                powers[count] = i;
+                        if (!inlist(i)){
+                                append(i, count); 
                                 count++;
                         }
 
                 }
         }
 
-//        long int length = len(powers);
         printf("%d\n", count);
-//        printf("%d\n", temp);
 
         return 0;
 
 }                        
 
 
-
-/* a to the power of b */
-unsigned long long power(int a, int b)
-{
-        if (b == 0){
-                return 1;
-        }
-
-        /* So we don't have to allocate 
-         * the memory if b == 0 */
-        register unsigned long long result = 1;
-
-        for(int i = 0; i < b; i++){
-                result *= a;
-        }
-
-        return result;
-        
-}
-
-
 /* Check if i in array */
-int inlist(long long int i, int count)
+int inlist(mpz_t i)
 {
-        int found = 0;
+	/* Removed sorting until the
+         * rest works 
 
-        /* Infinite loop... */
-        for(long int a = 0; a < count+1; a++){
+        int length = len(powers);
+        bubble_sort(powers, length);
+
+	*/
+
+        /* Temporary end cond, more efficient will be
+         * to insert elements in a sorted array */
+
+        for(long int a = 0; powers[a] != '\0'; a++){
                 /* Check if in list */
                 if (powers[a] == i){
-                        found = 1;
-                        break;
+                        printf("%d\n", powers[a]);
+                        return 1;
                 }
+
                 /* Assume to be sorted; if value is
                  * bigger than current item, not in 
                  * the list */
 
-/*
-                if (powers[a] < i){
-                        break;
-
-                }
-*/
+//                if (powers[a] > i){
+//                        return 0;
+ //               }
 
         } 
 
-        return found;
+        return 0;
 }
+
 
 /* Find length of list */
 long len(unsigned long int list[])
 {
         long int i;
 
-        for(i = 0; list[i] != '\0'; i++);
+        for(i = 0; list[i] != '\0'; i++)
+                ;
 
         return i;
 }
 
+
+/* Good for nearly sorted  array */
+void bubble_sort(long list[], long n)
+{
+ 	long c, d, t;
+ 
+	for (c = 0; c < (n - 1); c++){
+		for (d = 0; d < n - c - 1; d++){
+			if (list[d] > list[d + 1]){
+				t = list[d];
+				list[d] = list[d + 1];
+				list[d + 1] = t;
+                        }
+                }
+        }
+}
+
+void append(mpz_t i, int count){
+{
+        /* Something along these lines */
+        char string[1024];
+        mpz_get_str(i, string);
+        powers[count] = string;
+}

bdca3e4a20589143b26fe825bf5655ee208e27ae
Guy Nankivell <guy.nankivell@solnet.co.nz>
2016-12-23 15:39:55 +1300

    Things

---
+++ progress/dice_game.c
@@ -0,0 +1,24 @@
+/* Peter has nine four-sided (pyramidal) dice, 
+ * each with faces numbered 1, 2, 3, 4.
+ * Colin has six six-sided (cubic) dice, each 
+ * with faces numbered 1, 2, 3, 4, 5, 6.
+
+ * Peter and Colin roll their dice and compare totals: 
+ * the highest total wins. 
+ * The result is a draw if the totals are equal.
+
+ * What is the probability that Pyramidal Pete beats 
+ * Cubic Colin? Give your answer rounded to seven 
+ * decimal places in the form 0.abcdefg 
+
+ * Project Euler: 205 */
+
+
+#include <stdio.h>
+#include <math.h>
+
+/* We know max total for Peter and Colin is both 36
+ * whereas the minimum for Peter is 4 whereas for 
+ * Colin it is 6 */
+
+
---
+++ src/even_fib_2.c
@@ -0,0 +1,40 @@
+/* Find the sum of all the even values in the 
+ * Fibonacci sequence whose values do not 
+ * exceed four million. 
+ *
+ * Project Euler: 2 
+ * Answer: 4613736 */
+
+/* Includes */
+#include <stdio.h>
+
+/* Definitions */
+#define MAX 4000000
+
+/* Prototypes */
+int main();
+
+int main()
+{
+	int first = 1; int second = 1; int term; int sum = 4;
+
+	while(1){
+
+                term = first + second;
+
+		if(term > MAX){
+			break;
+		}
+
+		sum += (term % 2 == 0) ? term : 0;
+
+		first = second;
+		second = term;
+	}
+
+        printf("%d\n", sum);
+
+        return 0;
+
+}
+

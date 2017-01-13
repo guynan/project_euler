3e17eb161818de4381c9434634f291ac0bfc9ce5
Guy Nankivell <guy.nankivell@solnet.co.nz>
2017-01-13 15:55:40 +1300

    Quite shitty

--- progress/circular_primes.c
+++ progress/circular_primes.c
@@ -21,7 +21,10 @@ int res[500];
 /* Prototypes */
 int main();
 int isPrime(unsigned int s);
+int stringcomp(char this[], char that[]);
+void strprint(char string[]);
 void initPrimes();
+int len(char c[]);
 
 int main()
 {
@@ -54,6 +57,7 @@ int isPrime(unsigned int s)
                 return 1;
         }
 
+        /* Hopefully preventing rounding errors */
         int top = (int) round(sqrt(s) +1 );
 
         for(int i = 2; i < top+1; i++){
@@ -85,7 +89,108 @@ void initPrimes()
 void circulate(int i)
 {
         char n[10];
+        char f[10];
+
+        /* Create n as a an array of 
+         * characters */
+        sprintf(n, "%d", i);
+
+        /* While n != f */
+        for(int l = 0; !stringcomp(n,f); l++){
+                if(l + 1 > len(f)){
+                        l = 0;
+                }
+
+                f[l] = n[l + 1];
+        }
+}
 
 
 
+        
+
+}
+        
+/* This checks to see if `i` is
+ * in the list of generated primes */
+int inPrimes(int i)
+{
+        for(int c = 0; primes[c] != '\0'; c++){
+                if(i == primes[c]){
+                        return 1;
+                }
+                
+                if(i > primes[c]){
+                        return 0;
+                }
+        }
+
+        return 0;
+}
+
+
+/*
+int stringcomp(char this[], char that[])
+{
+        for(int i = 0; this[i] != '\0'; i++){
+                if(this[i] != that[i]){
+                        return 0;
+                }
+        }
+
+        return 1;
+}
+*/
+
+int len(char c[])
+{
+        int i;
+
+        for(c = 0; c[i] != '\0'; c++);
+
+        return c;
+}
+
+
+void circulate(int i)
+{
+        char strcp[10];
+        char f[10];
+        char tmp[10];
+
+        /* Create n as a an array of
+         * characters */
+        sprintf(strcp, "%d", i);
+
+        /* Copy the string into `tmp` */
+        for(int i = 0; strcp[i] != '\0'; tmp[i] = strcp[i], i++);
+
+        strprint(tmp);
+
+
+        /* While n != f */
+        for(int l = 0; !stringcomp(strcp ,f); l++){
+
+                for(int c = l; tmp[c] != '\0'; c++){
+
+                        if(f[c + 1] == '\0'){
+                                f[0] = tmp[c];
+                        }
+
+                        f[c] = tmp[c - 1];
+                }
+
+//                strprint(tmp);
+
+                for(int i = 0; tmp[i] != '\0'; tmp[i] = f[i], i++);
+
+                printf("\n");
+        }
+}
+
+void strprint(char string[])
+{
+
+        for(int m = 0; string[m] != '\0'; printf("%c", string[m]), m++);
+}
 

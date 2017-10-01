91e5c79a7deb278896b813d8ca96d83af7ba21fb
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2017-10-02 11:56:11 +1300

    This is done sneakily

--- src/consec_prime_sum.c
+++ src/consec_prime_sum.c
@@ -12,7 +12,9 @@
  * Which prime, below one-million, can be 
  * written as the sum of the most consecutive primes?
 
- * Project Euler: 50 */
+ * Project Euler: 50 
+
+ * Answer: 997651 */
 
 #include <inttypes.h>
 #include <stdlib.h>
@@ -23,74 +25,47 @@
 #define NUM_PRIMES      20000
 #define MAX             1000000
 
-int isIn(uint64_t sum, uint32_t* p);
+int isIn(uint64_t sum, uint32_t* p) __attribute((optimize(2)));
+uint32_t longestConsec(uint32_t* primes, uint32_t max);
 int main(int argc, char** argv);
-uint32_t* initPrimes(int max);
+uint32_t* initPrimes(uint32_t max);
 int isPrime(uint32_t s);
 
 
 int main(int argc, char** argv)
 {
-        /* One assumption I make is that the 
-         * longest consecutive sum will start
-         * from the first prime number; 2. */
+        (void) argc; 
+        (void) argv;
 
         uint32_t* primes = initPrimes(MAX);
 
-        uint64_t largest = 0;
-        uint64_t sum;
-        uint32_t length = 0;
-
-
-
-        /* Naive solution */
-        for(int i = 0; primes[i]; i++){
-                
-                sum = 0;
-
-                for(int j = i; primes[j]; j++){
-                        sum += primes[j];
-//                        printf("%"PRIu64"\n", sum);
-                        if(isIn(sum, (primes + j))){
-                                uint32_t diff = j - i;
-                                if(j - i > length){
-                                        largest = sum;
-                                        length = diff;
-                                }
-                        }
-                }
-        }
-
-
-        printf("%"PRIu64"\n", largest);
+        printf("%"PRIu32"\n", longestConsec(primes,  MAX));
 
         return 0;
 }
 
 
 /* Generate a list of primes up to 1mil */
-uint32_t* initPrimes(int max)
+uint32_t* initPrimes(uint32_t max)
 {
-        int n = NUM_PRIMES;
-        int count = 0;
+        uint32_t n = NUM_PRIMES;
+        uint32_t count = 0;
         uint32_t* p = malloc(n * sizeof(uint32_t));
 
-        for(int i = 0; i < max; i++){
+        for(uint32_t i = 0; i < max; i++){
 
                 if(isPrime(i)){
                         p[count] = i;
                         count++;
                 }
 
-                if(count == n){
-                        p = realloc(p, (n *= 2) * sizeof(uint32_t));
-                        puts("realloc");
-                }
+                p = (count == n) ? realloc(p, (n *= 2) * sizeof(uint32_t)) : p;
 
         }
 
         return p;
 }
+
 
 int isPrime(uint32_t s)
 {
@@ -121,5 +96,37 @@ int isIn(uint64_t sum, uint32_t* p)
         }
 
         return 0;
+
+}
+
+
+uint32_t longestConsec(uint32_t* primes, uint32_t max)
+{
+        uint32_t largest = 0;
+        uint32_t length = 0;
+
+        /* Naive solution */
+        for(int i = 0; primes[i]; i++){
+                
+                uint64_t sum = 0;
+
+                for(int j = i; primes[j]; j++){
+                        sum += primes[j];
+
+                        if(sum > max) break;
+
+                        if(isIn(sum, (primes + j))){
+                                uint32_t diff = j - i;
+
+                                if(diff > length){
+                                        largest = sum;
+                                        length = diff;
+                                }
+                        }
+                }
+        }
+
+        return largest;
+
 
 }

e640ef7b43bce0da4e1c81f335ee294d53adb1d8
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2017-10-16 16:26:16 +1300

    Getting closer

--- progress/palindromic_sums.c
+++ progress/palindromic_sums.c
@@ -9,7 +9,9 @@
 //#define LIMIT           1000
 #define BASE            10
 
-uint32_t generatePalindrome(uint32_t i);
+uint32_t generatePalindrome(uint32_t i, uint32_t flag);
+uint32_t generateSquares(uint32_t limit);
+uint32_t numLength(uint32_t i);
 int main(int argc, char** argv);
 uint32_t intrev(uint32_t i);
 uint32_t isSquareSum(uint32_t i);
@@ -29,9 +31,20 @@ int main(int argc, char** argv)
          * Since the range is so big and there are so few 
          * palindromes, it is far less computationally expensive
          * to instead generate palindromes and test those */
-
+        uint32_t p = 1;
+        /* R is a flag to designate whether we need to generate an odd
+         * or an even length palindrome. */
+        for(uint32_t r = 0; r < 2; r++){
+                for(uint32_t i = 1; ; i++){
+                        p = generatePalindrome(i, r);
+                        if(p > LIMIT) break;
+                        count++;
+                }
+        }
+        
 
         printf("%"PRIu64"\n", count);
+
         return 0;
 
 }
@@ -63,21 +76,70 @@ uint32_t isSquareSum(uint32_t i)
          */
 }
 
-uint32_t generatePalindrome(uint32_t i)
+uint32_t generatePalindrome(uint32_t i, uint32_t flag)
 {
-        uint32_t rev = intrev(i);
+        uint32_t rev = (i);
         uint32_t rem;
 
-        while(rev){
-                
-
-// Code Graveyard
-/*
-        for(uint32_t i = 0; i < LIMIT; i++){
-//                if(!isPalindrome(i)) continue;
-//                sum += isSquareSum(i);
-                count += isPalindrome(i);
-
+        if(flag){
+                i /= BASE;
         }
-*/
+
+        while(rev){
+                i = i * BASE + (rev % BASE);
+                rev /= BASE;
+        }
+
+        return i;
+
+}
+                
+uint32_t numLength(uint32_t i)
+{
+        uint32_t rem;
+        uint32_t base = BASE;
+        uint32_t n = 1;
+
+        while(i % base != i){
+                base *= 10;
+                n++;
+        }
+
+        return n;
+}
+
+uint32_t longestConsec(uint32_t* primes, uint32_t max)
+{
+        uint32_t largest = 0;
+        uint32_t length = 0;
+
+        /* Naive solution */
+        for(int i = 0; primes[i]; i++){
+                
+                uint64_t sum = 0;
+
+                for(int j = i; primes[j]; j++){
+                        sum += primes[j];
+
+                        if(sum > max) break;
+
+                        if(isIn(sum, (primes + j))){
+                                uint32_t diff = j - i;
+
+                                if(diff > length){
+                                        largest = sum;
+                                        length = diff;
+                                }
+                        }
+                }
+        }
+
+        return largest;
+
+
+}
+
+
+uint32_t generateSquares(uint32_t limit)
+{
 

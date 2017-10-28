c6fa194cdef252dfd8b25ffa862d372e7b697912
Guy Nankivell <guy.nankivell@solnet.co.nz>
2017-10-28 13:07:31 +1300

    This works optimisations to come

--- progress/sub_string_divis.c
+++ progress/sub_string_divis.c
@@ -1,3 +1,22 @@
+/* The number, 1406357289, is a 0 to 9 pandigital number because it is made up
+ * of each of the digits 0 to 9 in some order, but it also has a rather
+ * interesting sub-string divisibility property.
+ *
+ * Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we
+ * note the following:
+ *
+ * d2d3d4=406 is divisible by 2
+ * d3d4d5=063 is divisible by 3
+ * d4d5d6=635 is divisible by 5
+ * d5d6d7=357 is divisible by 7
+ * d6d7d8=572 is divisible by 11
+ * d7d8d9=728 is divisible by 13
+ * d8d9d10=289 is divisible by 17
+ * Find the sum of all 0 to 9 pandigital numbers with this property.
+ *
+ * Project Euler: 43 
+ *
+ * Answer: 16695334890 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,57 +34,36 @@
 static uint8_t PAND8_T[11] = {9,8,7,6,5,4,3,2,1};
 static int PRIMES[9] = {17, 13, 11, 7, 5, 3, 2, 1};
 
-int main(int argc, char** argv);
-int ccomp(const void* a, const void* b);
 int dcomp(const void* a, const void* b);
-int isPandigitalSO(uint64_t n);
-int isStrPandigital(uint64_t i);
-int isPandigital(uint64_t i);
+uint8_t isPandigital(uint64_t i);
 int isSubStringDivis(uint64_t n);
+int main(int argc, char** argv);
 
 
 int main(int argc, char** argv)
 {
         (void) argc;
         (void) argv;
+ 
         uint64_t sum = 0;
-        uint32_t count = 0;
-        for(uint64_t i = START; i < MAX; i++){
-//                if(i % 9 != 0) continue;
-                /*
-                if(isSubStringDivis(i) && isPandigitalSO(i)){
+
+        /* Okay so this loop has been optimised the shit out of */
+        for(uint64_t i = START; i < MAX; i+= 9){
+                if(isSubStringDivis(i) && isPandigital(i)){ 
+                        printf("%"PRIu64"\n", i);
                         sum += i;
                 }
-                */
-                if(isPandigitalSO(i)) count++;
-
         }
 
-        printf("%"PRIu32"\n", count);
         printf("%"PRIu64"\n", sum);
 
         return 0; 
 
 }
 
-int isStrPandigital(uint64_t i)
+
+uint8_t isPandigital(uint64_t i)
 {
-//        if(i % 9 != 0) return 0;
-
-        char* digit = calloc(PANLEN, sizeof(char));
-        sprintf(digit, "%"PRIu64, i);
-        qsort(digit, PANLEN, sizeof(char), ccomp);
-
-        int res = (0 == strcmp(digit, PANDIGIT));
-
-        free(digit);
-        return res;
-
-}
-
-int isPandigital(uint64_t i)
-{
-        if(i % 9 != 0) return 0;
         uint8_t* digit = calloc(PANLEN, sizeof(uint8_t));
 
         for(uint8_t j = 0; i > 0; j++){
@@ -74,14 +72,8 @@ int isPandigital(uint64_t i)
         }
 
         qsort(digit, PANLEN, sizeof(char), dcomp);
-        /*
-        for(uint8_t j = 0; j < 10; j++){
-                printf("%d", (int)digit[j]);
-        }
-        printf("\n");
-        */
 
-        int res = (0 == strcmp(digit, PAND8_T));
+        uint8_t res = (0 == strcmp(digit, PAND8_T));
 
         free(digit);
         return res;
@@ -93,36 +85,13 @@ int dcomp(const void* a, const void* b)
         return (*((uint8_t*) a) < *((uint8_t*) b));
 }
 
-int ccomp(const void* a, const void* b)
-{
-        return (*((char*) a) - *((char*) b));
-}
-
-
-/* This is a pandigital confirmation method lifted from Stack Overflow */
-int isPandigitalSO(uint64_t n)
-{
-        uint64_t digits = 0;
-        uint64_t count = 0;
-        uint64_t tmp;
-
-        for( ; n > 0; n /= 10, ++count){
-
-                if((tmp = digits) == (digits |= 1 << (n 
-                        - ((n / BASE) * BASE) - 1))) 
-                        return 0;
-
-        } 
-
-        return digits == (1 << count) - 1;
-
-}
 
 int isSubStringDivis(uint64_t n)
 {
-        for(uint8_t i = 0; n > 1000; i++){
-                uint32_t tmp = n % 1000;
+        uint16_t tmp;
 
+        for(uint8_t i = 0; n > 1000; i++){
+                tmp = (n % 1000);
                 if(tmp % PRIMES[i] != 0) return 0;
                 n /= 10;
         }
@@ -130,3 +99,4 @@ int isSubStringDivis(uint64_t n)
         return 1;
 
 }
+

81ced6e89060471ec8495e3b07d1136877312648
Guy Nankivell <guy.nankivell@solnet.co.nz>
2017-10-28 15:19:04 +1300

    This is done

--- src/sub_string_divis.c
+++ src/sub_string_divis.c
@@ -26,14 +26,16 @@
 
 
 #define START           1023456789
-#define MAX             9876543299
-#define PANDIGIT        "9876543210"
+#define MAX             4999999999
 #define PANLEN          16
 #define BASE            10
 
-static uint8_t PAND8_T[11] = {9,8,7,6,5,4,3,2,1};
-static int PRIMES[9] = {17, 13, 11, 7, 5, 3, 2, 1};
 
+static uint8_t PAND8_T[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+static const int PRIMES[] = {17, 13, 11, 7, 5, 3, 2};
+
+
+uint8_t shortcmp(uint8_t *a, uint8_t *b);
 int dcomp(const void* a, const void* b);
 uint8_t isPandigital(uint64_t i);
 int isSubStringDivis(uint64_t n);
@@ -45,20 +47,43 @@ int main(int argc, char** argv)
         (void) argc;
         (void) argv;
  
+        /* A couple of things that speed up our computation:
+         *
+         *      All pandigital numbers (0-9) are divisible by 9 so we can 
+         *      increment by 9 to reduce our search space. We start with the 
+         *      first known pandigital number, so we know each subsequently 
+         *      generated number is far more likely to be one too.
+         *
+         *      I have found that it is also considerably more computationally
+         *      efficient to first check if the number is substring divisible
+         *      before checking whether it is a pandigit. This is because all
+         *      numbers --irrespective of their unsuitability-- must be checked
+         *      and my algorithm for checking if it is pandigital relies on 
+         *      sorting the number first even before any suitability checking 
+         *      can commence. This means that it takes O(n log n) time. */
+
         uint64_t sum = 0;
 
-        /* Okay so this loop has been optimised the shit out of */
         for(uint64_t i = START; i < MAX; i+= 9){
-                if(isSubStringDivis(i) && isPandigital(i)){ 
-                        printf("%"PRIu64"\n", i);
-                        sum += i;
-                }
+                if(isSubStringDivis(i) && isPandigital(i)) sum += i;
         }
 
         printf("%"PRIu64"\n", sum);
 
         return 0; 
 
+}
+
+
+uint8_t shortcmp(uint8_t *a, uint8_t *b)
+{
+        uint8_t i = 0;
+
+        for( ; a[i]; i++){
+                if(a[i] != b[i]) return 0;
+        }
+
+        return (b[i] == '\0');
 }
 
 
@@ -71,14 +96,16 @@ uint8_t isPandigital(uint64_t i)
                 i /= BASE;
         }
 
-        qsort(digit, PANLEN, sizeof(char), dcomp);
+        qsort(digit, PANLEN, sizeof(uint8_t), dcomp);
 
-        uint8_t res = (0 == strcmp(digit, PAND8_T));
+        uint8_t res = shortcmp(digit, PAND8_T);
 
         free(digit);
+
         return res;
 
 }
+
 
 int dcomp(const void* a, const void* b)
 {
@@ -88,12 +115,16 @@ int dcomp(const void* a, const void* b)
 
 int isSubStringDivis(uint64_t n)
 {
-        uint16_t tmp;
+        static uint16_t tmp;
 
-        for(uint8_t i = 0; n > 1000; i++){
+        for(uint8_t i = 0; n > 1000; ){
+
                 tmp = (n % 1000);
-                if(tmp % PRIMES[i] != 0) return 0;
+
+                if(tmp % PRIMES[i++] != 0) return 0;
+
                 n /= 10;
+                
         }
 
         return 1;

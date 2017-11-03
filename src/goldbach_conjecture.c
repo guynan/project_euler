e3ca7c2ca2bccdd616e556f96d88dfb4e35a7290
Dan Richards <dan.c.richards@gmail.com>
2017-11-03 14:18:41 +1300

    Fixed formatting issues with brackets
    Jesus, it's almost as if you're writing this in Java.....

--- src/goldbach_conjecture.c
+++ src/goldbach_conjecture.c
@@ -1,12 +1,12 @@
 /* It was proposed by Christian Goldbach that every odd composite number can be
  * written as the sum of a prime and twice a square.
  *
- * 9 = 7 + 2×1**2
- * 15 = 7 + 2×2**2
- * 21 = 3 + 2×3**2
- * 25 = 7 + 2×3**2
- * 27 = 19 + 2×2**2
- * 33 = 31 + 2×1**2
+ * 9 = 7 + 2Ã—1**2
+ * 15 = 7 + 2Ã—2**2
+ * 21 = 3 + 2Ã—3**2
+ * 25 = 7 + 2Ã—3**2
+ * 27 = 19 + 2Ã—2**2
+ * 33 = 31 + 2Ã—1**2
  *
  * It turns out that the conjecture was false.
  *
@@ -44,7 +44,8 @@ int main(int argc, char** argv)
         for(i = 3; i < MAX; i += 2){
 
                 int found = 1;
-                for(uint32_t j = 0; i >= primes[j]; j++){
+                for(uint32_t j = 0; i >= primes[j]; j++)
+                {
 
                         if(isSquare(i - primes[j])){
                                 found = 0;

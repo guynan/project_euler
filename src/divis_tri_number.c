a68fc7e37f3e8f9e7ca349ef293b0c4e0f8533a4
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2018-03-16 14:27:46 +1300

    Cleaning out cruft

--- src/divis_tri_number.c
+++ src/divis_tri_number.c
@@ -1,29 +1,25 @@
 /* Find the first triangle number with over
- * 500 divisors. Fuck this is big.
+ * 500 divisors.
  * Answer: 76576500
  *
  * Project Euler: 12 */
 
-/* Includes */
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
 
 
-/* Definitions */
 #define MAX 500
 #define MAXLEN 50000
 
 
-/* Prototypes */
-uint64_t maxTriDivis(uint32_t max);
+uint64_t maxTriDivis(uint64_t max);
 int main(int argc, char** argv);
-uint32_t numdivis(uint64_t f);
+uint64_t numdivis(uint64_t f);
 
 
 int main(int argc, char** argv)
 {
-
         (void) argc;
         (void) argv;
 
@@ -32,27 +28,25 @@ int main(int argc, char** argv)
         return 0;
 }
 
-
-uint64_t maxTriDivis(uint32_t max)
+uint64_t maxTriDivis(uint64_t max)
 {
-        uint64_t last = 3;
+        uint64_t tri = 3;
 
-        for(uint64_t i = 3; ; last += i++){
+        for(uint64_t i = 3; ; tri += i++){
                 
-                if (numdivis(last) > max) break;
+                if (numdivis(tri) > max)
+                        break;
         }
 
-        return last;
+        return tri;
 }
 
-        
-uint32_t numdivis(uint64_t f)
+uint64_t numdivis(uint64_t f)
 {
-        uint32_t count = 1;
+        uint64_t count = 1;
 
-        for(uint32_t i = 1; i*i < f; i++){
-                count += (f % i == 0);
-        }
+        for(uint64_t i = 1; i*i < f; count += (f % i++ == 0))
+                ;
 
         return 2 * count;
 

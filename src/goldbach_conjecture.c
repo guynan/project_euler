449effb8a729c5c412d8e3d690efe078c929b164
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2018-03-16 14:11:53 +1300

    Was fast -- now looks better

--- src/goldbach_conjecture.c
+++ src/goldbach_conjecture.c
@@ -23,15 +23,16 @@
 #include <math.h>
 #include <inttypes.h>
 
+
 #define MAX             10000
 
-/* Prototypes */
+
 uint32_t findClosestPrime(uint32_t i, uint32_t* primes);
 uint32_t* initPrimes(uint32_t max);
 int main(int argc, char** argv);
-int obeysGoldbach(long i);
 int isPrime(uint32_t s);
 int isSquare(uint32_t);
+
 
 int main(int argc, char** argv)
 {
@@ -39,46 +40,44 @@ int main(int argc, char** argv)
         (void) argv;
 
         uint32_t* primes = initPrimes(MAX);
-        uint32_t i;
 
-        for(i = 3; i < MAX; i += 2){
+        uint32_t i = 0;
+
+        if(!primes){
+                return -1;
+        }
+
+        for(i = 3; ; i += 2){
 
                 int found = 1;
+
                 for(uint32_t j = 0; i >= primes[j]; j++){
 
                         if(isSquare(i - primes[j])){
                                 found = 0;
                                 break;
                         } 
-
                 }
-                if(found) goto done;
+
+                if(found){
+                        goto done;
+                }
         }
 
-                
-        done:
-                printf("%"PRIu32"\n", i);
+done:
+        free(primes);
+        printf("%"PRIu32"\n", i);
 
         return 0;
 }
-
-int obeysGoldbach(long i)
-{
-        return 0;
-}
-
 
 int isPrime(uint32_t s)
 {
-        /* Theoretically shouldn't get values
-         * less than zero due to type safety */
         if (s < 2) return 0;
-        if(s == 2) return 1;
 
-        /* Preventing rounding errors */
         uint32_t top = (uint32_t) round(sqrt(s) + 1);
 
-        for(uint32_t i = 2; i < top + 1; i++){
+        for(uint32_t i = 2; i <= top; i++){
 
                 if(i == top) return 1;
 
@@ -88,36 +87,28 @@ int isPrime(uint32_t s)
         return 0;
 }
 
+/* This is fucking dirty */
 int isSquare(uint32_t a)
 {
         double tmp = sqrt((double) a / 2);
         return ((uint32_t) tmp == tmp);
-
-}
-
-
-uint32_t findClosestPrime(uint32_t i, uint32_t* primes)
-{
-        uint32_t largest = 0;
-
-        for(uint32_t j = 0; i <= primes[j]; j++){
-                largest = primes[j];
-        }
-
-        return largest;
-
 }
 
 uint32_t* initPrimes(uint32_t max)
 {
-
         uint32_t* primes = malloc(MAX * sizeof(uint32_t));
+
+        if(!primes){
+                goto out;
+        }
 
         uint32_t c = 0;
         for(uint32_t i = 0; i < max; i++){
                 if(isPrime(i)) primes[c++] = i;
         }
 
+out:
         return primes;
 
 }
+

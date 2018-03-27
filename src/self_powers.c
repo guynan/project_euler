b3098a9a96d2fb6a3d74279cb59ae76f834fff81
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2018-03-28 10:58:17 +1300

    Minor refactor

--- src/self_powers.c
+++ src/self_powers.c
@@ -2,27 +2,30 @@
  * Find the last ten digits
  * of the sum of this series
  *
- * Answer: 9110846700 
+ * Answer: 9110846700
  * Project Euler: 48 */
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <gmp.h>
 
 
 #define MAX 1000
-mpz_t sum;
-mpz_t power;
-char string[4096];
 
 
-int main();
-int len(char *string);
+int main(int argc, char** argv);
 
 
-int main()
+int main(int argc, char** argv)
 {
-        mpz_init(sum); mpz_init(power);
+        (void) argc;
+        (void) argv;
+
+        mpz_t sum, power;
+        mpz_init(sum);
+        mpz_init(power);
 
         unsigned long int i;
 
@@ -30,24 +33,14 @@ int main()
                 mpz_ui_pow_ui(power, i, i);
                 mpz_add(sum, sum, power);
         }
-        
-        mpz_get_str(string, 10, sum); 
-        int length = len(string);
 
-        for(int c = length - 10; c <= length; c++){
-                printf("%c", string[c]);
-        }
+        char* str = mpz_get_str(NULL, 10, sum);
 
-        printf("\n");
+        printf("%s\n", str + (strlen(str) - 10));
+
+        mpz_clears(sum, power, NULL);
+        free(str);
 
         return 0;
 }
 
-int len(char *string)
-{
-        int i = 0;
-
-        for(; string[i] != '\0'; i++);
-
-        return i;
-}

7dbf4b7ef6785f0b1a5dce8bec855d66378f338b
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2018-03-28 15:07:55 +1300

    Much cleaner than the previous implementation

--- src/permuted_multiples.c
+++ src/permuted_multiples.c
@@ -1,34 +1,42 @@
-/* It can be seen that the number, 125874, 
+/* It can be seen that the number, 125874,
  * and its double, 251748, contain
  * exactly the same digits, but in a different order.
 
- * Find the smallest positive integer, x, such that 
+ * Find the smallest positive integer, x, such that
  * 2x, 3x, 4x, 5x, and 6x, contain the same digits.
 
- * Project Euler: 52 
+ * Project Euler: 52
  * Answer: 142857 */
 
 
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <math.h>
 
 
-int contains(char *string, char i);
-int permuted(int i, int compare);
-int intLen(int i);
-int valid(int i);
-int main();
+#define BASE                    10
+#define PERMUTE_MULTIPLIER      6
 
 
-int main()
+int mult_permutation(int32_t i, int32_t max);
+int permuted(int32_t a ,int32_t b);
+int main(int argc, char** argv);
+int32_t sum_digits(int32_t a);
+
+
+int main(int argc, char** argv)
 {
-        int i;
-        for(i = 1; ; i++){
-                if(valid(i)) break;
-        }
+        (void) argc;
+        (void) argv;
 
-        printf("%d\n", i);
+        int32_t i;
+
+        for(i = 1; !mult_permutation(i, PERMUTE_MULTIPLIER); i++)
+                ;
+
+        printf("%"PRIu32"\n", i);
 
         return 0;
 }
@@ -36,15 +44,22 @@ int main()
 
 /* Checks if the integer is valid by comparing
  * it against multiples of itself. */
-int valid(int i)
+int mult_permutation(int32_t i, int32_t max)
 {
-        int compare;
-        for(int c = 2; c < 7; c++){
+        int32_t compare;
+
+        for(int32_t c = 2; c <= max; c++){
                 compare = i * c;
 
-                if(intLen(i) != intLen(compare)) return 0;
+                /* The majority will not be even remotely similar. This
+                 * calculation can be done very swiftly and reduced the search
+                 * space for more expensive calculations. This can also be done
+                 * whilst keeping the instruction pipline pretty hot. */
+                if(sum_digits(i) != sum_digits(compare))
+                        return 0;
 
-                if(!permuted(i , compare)) return 0;
+                if(!permuted(i , compare))
+                        return 0;
         }
 
         return 1;
@@ -53,37 +68,46 @@ int valid(int i)
 
 /* Checks if the integer is a permutation
  * of itself multiplied by another integer */
-int permuted(int i, int compare)
+int permuted(int32_t a, int32_t b)
 {
-        char *iptr = malloc(16 * sizeof(char));
-        char *compptr = malloc(16 * sizeof(char));
+        int32_t* instances = calloc(BASE, sizeof(int32_t));
 
-        sprintf(iptr, "%d", i);
-        sprintf(compptr, "%d", compare);
-
-        for( ; *iptr; ){
-                if(!contains(compptr, *iptr++)) return 0;
+        for( ; a; a /= BASE){
+                (instances[a % BASE])++;
         }
 
-        return 1;
+        for( ; b; b /= BASE){
+                (instances[b % BASE])--;
+        }
+
+        /* a is now a throwaway variable and already set to 0 and come to think
+         * of it, so is b so we can use this to store the return value */
+        for(a = 0; a < BASE; a++){
+
+                if(instances[a] != 0){
+                        goto out;
+                }
+        }
+
+        b = 1;
+
+out:
+
+        free(instances);
+        return b;
 
 }
 
 
-/* Iterates through string to see if
- * it is indeed in there */
-int contains(char *string, char i)
+int32_t sum_digits(int32_t i)
 {
-        for( ; *string != '\0'; ){
-                if(*string++ == i) return 1;
+        int32_t sum = 0;
+
+        for( ; i; i /= BASE){
+                sum += (i % BASE);
         }
 
-        return 0;
-}
+        return sum;
 
-
-int intLen(int i)
-{
-        return (floor(log10(abs(i))) + 1);
 }
 

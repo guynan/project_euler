fa75e8485e8f5fbc094c384b700444be3547e0fa
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2018-04-24 21:54:35 +1200

    So nice and speedy now

--- src/circular_primes.c
+++ src/circular_primes.c
@@ -1,5 +1,5 @@
-/* The number, 197, is called a circular prime 
- * because all rotations of the digits: 
+/* The number, 197, is called a circular prime
+ * because all rotations of the digits:
  * 197, 971, and 719, are themselves prime.
 
  * There are thirteen such primes below 100:
@@ -7,8 +7,8 @@
 
  * How many circular primes are there below one million?
 
- * Project Euler: 35 
- * Answer: 55 */ 
+ * Project Euler: 35
+ * Answer: 55 */
 
 
 /* Includes */
@@ -21,149 +21,100 @@
 
 /* Definitions */
 #define MAX             1000000
-#define PRIME_C         80000 
-
-static uint32_t result[60];
-static uint32_t resc = 0;
+#define BASE            10
 
 
 /* Prototypes */
-int in(uint32_t i, uint32_t *array);
-uint32_t oneRotation(uint32_t i);
 int main(int argc, char** argv);
-void append(uint32_t* array);
-void circulate(uint32_t i);
+uint32_t circulate(uint32_t i);
+int possibly_circular(uint32_t i);
+int circular_prime(uint32_t i);
 int isPrime(uint32_t s);
 
 
 int main(int argc, char** argv)
 {
-        for(uint32_t i = 1; i < MAX; i+= 2){
-                circulate(i);
-                
+        /* Count is 1 so we capture 2 */
+        uint32_t c = 1;
+
+        for(uint32_t i = 3; i < MAX; i+= 2){
+
+                if(circular_prime(i))
+                        c++;
         }
 
-        int counter = 0;
-        for(uint32_t i = 0 ; i < 60; i++){
-                if(result[i] != 0){
-                        counter++;
-                }
-        }
-
-        printf("%d\n", counter + 1);
+        printf("%"PRIu32"\n", c);
 
         return 0;
-        
-}        
+
+}
 
 
 /* Checks if an integer is a prime
  * in the most efficient way I know */
 int isPrime(uint32_t s)
 {
-        if (s <= 1) return 0;
+        if (s < 2)
+                return 0;
 
-        if (s == 2) return 1;
+        uint32_t top = (uint32_t) round((double) sqrt(s));
 
-        /* Hopefully preventing rounding errors */
-        int top = (int) round(sqrt(s) + 1);
+        for(uint32_t i = 2; i <= top; i++){
 
-        for(int i = 2; i < top + 1; i++){
-                if(i == top) return 1;
-
-                if (s % i == 0) return 0;
+                if (s % i == 0)
+                        return 0;
         }
 
-        return 0;
+        return 1;
 }
 
 
-/* A real monty of a function:
- * We first have a look through all possible 
- * circular iterations of the integer `i`. 
- * If they are all prime numbers themselves; 
- * we chuck them in the `result` array using `append` */
-void circulate(uint32_t i)
+int circular_prime(uint32_t i)
 {
-        uint32_t rotated, count = 0;
-        uint32_t *circular = malloc(16 * sizeof(uint32_t));
+        if(!possibly_circular(i) || !isPrime(i))
+                return 0;
 
-        /* This is so we dont start rotating
-         * one index uint32_tegers */
-        if(i < 10){
-                if(isPrime(i)) result[resc++] = i;
-                return;
+        uint32_t rotated = i;
+
+        while(1){
+
+                rotated = circulate(rotated);
+                if(!isPrime(rotated))
+                        return 0;
+
+                if(i == rotated)
+                        return 1;
         }
 
-        /* Do one rotation so we can enter the loop */
-        rotated = oneRotation(i);
-
-        /* Return if it isn't even a prime */
-        if(!isPrime(i)){
-                return;
-        }
-
-        else circular[count++] = i;
-                
-        /* While rotated value != i */
-        while(i != rotated){
-
-                if(isPrime(rotated)){
-                        rotated = oneRotation(rotated);
-                        circular[count++] = rotated;
-                }
-
-                else return;
-                        
-        }
-
-        append(circular);
-        free(circular);
+        return 1;
 
 }
 
 
-uint32_t oneRotation(uint32_t i)
+uint32_t circulate(uint32_t i)
 {
+        if(i < BASE)
+                return i;
+
         uint32_t len = floor(log10(i));
-        uint32_t first = i % 10;
-        i /= 10;
-        return pow(10, len) * first + i;
+        uint32_t first = i % BASE;
+        i /= BASE;
+        return pow(BASE, len) * first + i;
 }
 
 
-/* Inserts the items of the array into the `result` array */
-void append(uint32_t* tmp)
+int possibly_circular(uint32_t i)
 {
-        uint32_t* array = tmp;
-        /* Only need to check if one item is
-         * in the list to know all the circular
-         * primes are indeed in the list */
-        if(in(*array, result)){
-                return;
+        while(i){
+
+                if((i % BASE) % 2 == 0)
+                        return 0;
+
+                i /= BASE;
+
         }
 
-        int value;
+        return 1;
 
-        /* Inserts unordered into `result` */
-        while(*array != '\0'){
-                value = *array++;
-                if(!in(value, result)){
-                        result[resc++] = value;
-                }
-        }
-
-}
-
-/* Pretty simple search; could make this faster by
- * appending in order and be able to break earlier */
-int in(uint32_t i, uint32_t* array)
-{
-        uint32_t* tmp = array;
-        while(*tmp != '\0'){
-                if(i == *tmp++) return 1;
-        }
-
-        return 0;
 }
 

75b512dec2e0d722ee5f0c895b221d36f13deb12
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2018-04-30 13:22:30 +1200

    0.956u -> 0.028. Big speed boost

--- src/quadratic_primes.c
+++ src/quadratic_primes.c
@@ -4,7 +4,7 @@
  *
  * Answer: -59231 */
 
-/* Includes */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -12,37 +12,32 @@
 #include <math.h>
 
 
-/* Definitions */
-#define MAX             1000
-#define MIN             -999
-#define MAX_PRIME       875000
-#define SIZEOF_PRIMES   75000
+#define MAX                     1000
+#define MIN                     -999
+#define QUAD_F(a, b, n)         (pow((n), 2) + ((a) * (n)) + (b))
 
 
-/* Function Protoytpes */
-int consecPrimes(int16_t a, int16_t b, int *primes);
-uint8_t in(int value, int *primes);
-uint8_t isPrime(uint32_t s);
-uint32_t mod(int i);
-int * initPrimes();
-int main();
+int32_t consecPrimes(int16_t a, int16_t b);
+int main(int argc, char** argv);
+int isPrime(int32_t s);
 
 
-int main()
+int main(int argc, char** argv)
 {
+        (void) argc;
+        (void) argv;
+
         int16_t a, b, chain;
 
-        /* Saves all the attributes about longest array 
-         * so if you want to find out about it you don't 
+        /* Saves all the attributes about longest array
+         * so if you want to find out about it you don't
          * need an unwieldly number of extra variables */
-        uint32_t *longest = malloc(3 * sizeof(uint32_t));
+        int16_t *longest = calloc(3, sizeof(int16_t));
 
-        /* Initialise our list of primes */
-        int *primes = initPrimes();
+        for(a = MIN; a < MAX; a += 2){
+                for(b = a; b < MAX; b += 2){
+                        chain = consecPrimes(a, b);
 
-        for(a = MIN; mod(a) < MAX; a++){
-                for(b = MIN; mod(b) < MAX; b++){
-                        chain = consecPrimes(a, b, primes);
                         if(chain > *longest){
                                 longest[0] = chain;
                                 longest[1] = a;
@@ -51,87 +46,52 @@ int main()
                 }
         }
 
-        printf("%d\n", longest[1] * longest[2]);
+        printf("%"PRId32"\n", (int32_t) longest[1] * longest[2]);
 
+        free(longest);
 
-}
+        return 0;
 
-/* Return modulus of int */
-uint32_t mod(int i)
-{
-        return (i < 0) ? -i : i;
 }
 
 
 /* Detects whether int is prime */
-uint8_t isPrime(uint32_t s)
+int isPrime(int32_t s)
 {
-        if (s < 1) return 0;
+        if (s < 2)
+                return 0;
 
-        if (s == 2) return 1;
+        int32_t top = (int32_t) round((float) sqrt(s));
 
-        int top = (int) round(sqrt(s) +1);
+        for(int32_t i = 2; i <= top; i++){
 
-        for(int i = 2; i < top+1; i++){
-                if(i == top) return 1;
-
-                if (s % i == 0) return 0;
+                if (s % i == 0)
+                        return 0;
         }
 
-        return 0;
+        return 1;
 }
 
 
-/* Counts how many primes can be generated using 
- * this sequence of f(a, b). Uses the prime array to 
+/* Counts how many primes can be generated using
+ * this sequence of f(a, b). Uses the prime array to
  * pass to check if the number we are testing is prime */
-int consecPrimes(int16_t a, int16_t b, int *primes)
+int32_t consecPrimes(int16_t a, int16_t b)
 {
-        uint32_t count = 0; uint32_t value;
+        int32_t count = 0;
+        int32_t tmp;
 
-        for(int n = 0; ; n++){
-                value = pow(n, 2) + (a * n) + b;
+        for(int32_t n = 1; ; n += 2){
 
-                if(in(value, primes)){
+                tmp = QUAD_F(a, b, n);
+
+                if(isPrime(tmp))
                         count++;
-                } else {
-                        return count;
-                }
-
+                else
+                        break;
         }
 
         return count;
-
-}
-
-
-/* Initialises a list of primes and returns it */
-int * initPrimes()
-{
-        int *primes = malloc(SIZEOF_PRIMES * sizeof(int));
-
-        for(uint32_t c, i = 0; i < MAX_PRIME; i++){
-
-                if(isPrime(i)){
-                        primes[c++] = i;
-                }
-        }
-
-        return primes;
-}
-
-
-uint8_t in(int value, int *primes)
-{
-
-        while(*primes){
-
-                if(*primes == value) return 1;
-
-                if(*primes++ > value) return 0;
-        }
-
-        return 0;
 
 }
 

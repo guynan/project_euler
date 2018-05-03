369ea106af5134d8fa52896e773e6405c4f6ea8c
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2018-05-04 10:36:35 +1200

    Wowo speed

--- src/largest_sum_series.c
+++ src/largest_sum_series.c
@@ -1,23 +1,46 @@
 /* Largest Sum in a series
- * Find the thirteen adjacent digits 
+ * Find the thirteen adjacent digits
  * in the 1000-digit number that have the
  * greatest product. What is the value of this product?
 
  * Answer: 23514624000
  * Project Euler: 9 */
 
+
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
 
 
-static char numstr[1024] = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
-int32_t number[1024];
+#define MAX             987
+#define LEN_ADJ         13
 
 
-void numconv(char* numstr, int32_t* num);
+static char numstr[1024] =
+                        "73167176531330624919225119674426574742355349194934"
+                        "96983520312774506326239578318016984801869478851843858"
+                        "61560789112949495459501737958331952853208805511125406"
+                        "98747158523863050715693290963295227443043557668966489"
+                        "50445244523161731856403098711121722383113622298934233"
+                        "80308135336276614282806444486645238749303589072962904"
+                        "91560440772390713810515859307960866701724271218839987"
+                        "97908792274921901699720888093776657273330010533678812"
+                        "20235421809751254540594752243525849077116705560136048"
+                        "39586446706324415722155397536978179778461740649551492"
+                        "90862569321978468622482839722413756570560574902614079"
+                        "72968652414535100474821663704844031998900088952434506"
+                        "58541227588666881164271714799244429282308634656748139"
+                        "19123162824586178664583591245665294765456828489128831"
+                        "42607690042242190226710556263211111093705442175069416"
+                        "58960408071984038509624554443629812309878799272442849"
+                        "09188845801561660979191338754992005240636899125607176"
+                        "06058861164671094050775410022569831552000559357297257"
+                        "1636269561882670428252483600823257530420752963450"
+;
+
+
+void numconv(char* numstr);
 int main(int argc, char** argv);
-int32_t ctoint(char c);
 
 
 int main(int argc, char** argv)
@@ -25,47 +48,51 @@ int main(int argc, char** argv)
         (void) argc;
         (void) argv;
 
-        numconv(numstr, number);
+        int64_t largest = 0;
+        int64_t prod;
+        char* tmp;
 
-        uint64_t largest = 0;
-        uint64_t prod;
-        int32_t x;
+        numconv(numstr);
 
-        for(int32_t i = 0; numstr[(i + 13)] != '\0'; i++){
+        for(int32_t i = 0; i < MAX; i++){
+
                 prod = 1;
-                for(int32_t local = i; local < (i + 13); local++){
-                        x = number[local];
+                tmp = numstr + i;
 
-                        /* If 0; won't be largest */
-                        if(x == 0){
+                for(int32_t j = 0; j < LEN_ADJ; j++){
+
+                        /* We know that if the current number is zero, that
+                         * means that at least 12 of the next products of the
+                         * series starting from index i will all evaluate to
+                         * zero. This is a tricky one to evaluate whether the
+                         * branch is worthwhile *or* it is better to keep the
+                         * instruction pipeline very hot. Testing
+                         * inconclusive */
+
+                        if(!*tmp){
+                                i += 12;
                                 break;
                         }
 
-                        prod *= x;
-                        largest = (prod > largest) ? prod : largest;
+                        prod *= *tmp++;
                 }
+
+                largest = (prod > largest) ? prod : largest;
 
         }
 
-        printf("%"PRIu64"\n", largest);
+        printf("%"PRId64"\n", largest);
 
         return 0;
 }
 
 /* Converts string to array of integers
  * as this yields 13x fewer conversions */
-void numconv(char* numstr, int32_t* num)
+void numconv(char* numstr)
 {
-        for(int i = 0; numstr[i] != '\0'; i++){
-                num[i] = ctoint(numstr[i]);
-        }
-}
+        for( ; *numstr; *numstr++ -= '0')
+                ;
 
-
-/* Manual str to int conversion */
-int32_t ctoint(char c)
-{
-        int32_t x = (int32_t) c - 48;
-        return x;
+        return;
 }
 

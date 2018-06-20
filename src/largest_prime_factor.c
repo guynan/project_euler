5a8f6ebfb18edd92a13c48ed1c106b7407264c9b
Guy Nankivell <guy.nankivell@solnet.co.nz>
2018-06-21 10:00:21 +1200

    Genuinely some of the least readable for loops

--- src/largest_prime_factor.c
+++ src/largest_prime_factor.c
@@ -1,49 +1,44 @@
 /* Find largest prime factor of the
- * number 600851475143 
+ * number 600851475143
  *
  * Answer: 6857
  * Project Euler: 3*/
 
-/* Includes */
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <math.h>
 
-/* Definitions */
-uint64_t MAX = 600851475143;
+int64_t MAX = 600851475143;
 
-/* Prototypes */
-int main();
-int isPrime(uint32_t s);
+int main(int argc, char** argv);
+int is_prime(int32_t s);
 
-int main()
+int main(int argc, char** argv)
 {
-        uint64_t largest; 
+        (void) argc;
+        (void) argv;
 
-        for(uint64_t i = 1; i * i < MAX; i++){
-                largest = (MAX % i == 0 && isPrime(i)) ? i : largest;
-        }
+        int32_t largest = 0;
 
-        printf("%li\n", largest);
+        int32_t limit = (int32_t) round(sqrt((double) MAX));
+
+        for(int32_t i = 1; i <= limit; i += ((largest = (MAX % i == 0 && is_prime(i)) ? i : largest), 1))
+                ;
+
+        printf("%"PRId32"\n", largest);
 }
 
 
 /* Detects whether int is prime */
-int isPrime(uint32_t s)
+int is_prime(int32_t s)
 {
-	if(s < 1) return 0;
+        int32_t res = !(s <= 2 || s % 2 == 0);
+        int32_t top = (int32_t) round(sqrt((float) s));
 
-        uint32_t top = (uint32_t) round(sqrt(s));
+        for(int32_t i = 3; i <= top && res; i += ((res = !(s % i == 0)), 2))
+                ;
 
-        for(uint32_t i = 2; i < top +1; i++){
-
-                if(i == top) return 1;
-
-                if (s % i == 0) return 0;
-        }
-
-        return 0;
-
+        return (res || s == 2);
 }
-
 

12b19340b853fbe0c37d011f47b17ca3a08e3904
Guy Nankivell <guy.nankivell@solnet.co.nz>
2018-06-27 09:26:48 +1200

    Roughly 3x faster

--- src/summation_primes.c
+++ src/summation_primes.c
@@ -1,56 +1,46 @@
 /* Finding the summation of primes < 2mil
- * This takes fucking ages
  *
- * Answer: 142913828922 
+ * Answer: 142913828922
  *
  * Project Euler: 10 */
 
-/* Includes */
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
 #include <math.h>
 
-/* Definitions */
 #define MAX 2000000
 
-/* Prototypes */
-int isPrime(uint32_t s);
 int main(int argc, char** argv);
+int is_prime(int32_t s);
+
 
 int main(int argc, char** argv)
 {
         (void) argc;
         (void) argv;
 
-        uint64_t sum = 2;
+        int64_t sum = 2;
 
-        for(uint32_t i = 3; i < MAX; i += 2){
+        for(int32_t i = 3; i < MAX; i += 2){
 
-                if (isPrime(i)) sum += i;
+                sum += (is_prime(i)) ? i : 0;
         }
 
-        printf("%"PRIu64"\n", sum);
-
-}
-       
-
-int isPrime(uint32_t s)
-{
-        /* Theoretically shouldn't get values
-         * less than zero due to type safety */
-        if (s < 2) return 0;
-
-        /* Preventing rounding errors */
-        uint32_t top = (uint32_t) round(sqrt(s) + 1);
-
-        for(uint32_t i = 2; i < top + 1; i++){
-
-                if(i == top) return 1;
-
-                if(s % i == 0) break;
-        }
+        printf("%"PRId64"\n", sum);
 
         return 0;
+
+}
+
+int is_prime(int32_t s)
+{
+        int32_t res = !(s <= 2 || (s & 0x1) != 1);
+        int32_t top = (int32_t) round(sqrt((float) s));
+
+        for(int32_t i = 3; res && i <= top; i += ((res = !(s % i == 0)), 2))
+                ;
+
+        return (res || s == 2);
 }
 

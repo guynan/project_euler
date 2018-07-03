2b025fbc8062f870f86f7d1264465b5cac0d1433
Guy Nankivell <guy.nankivell@solnet.co.nz>
2018-07-04 09:48:52 +1200

    This is done && faster than C impl

---
+++ fortran/spiral_primes.f95
@@ -0,0 +1,75 @@
+
+! Starting with 1 and spiralling anticlockwise in the following way, a square
+! spiral with side length 7 is formed.
+!
+! 37 36 35 34 33 32 31
+! 38 17 16 15 14 13 30
+! 39 18  5  4  3 12 29
+! 40 19  6  1  2 11 28
+! 41 20  7  8  9 10 27
+! 42 21 22 23 24 25 26
+! 43 44 45 46 47 48 49
+!
+! It is interesting to note that the odd squares lie along the bottom right
+! diagonal, but what is more interesting is that 8 out of the 13 numbers lying
+! along both diagonals are prime; that is, a ratio of 8/13  62%.
+!
+! If one complete new layer is wrapped around the spiral above, a square
+! spiral with side length 9 will be formed. If this process is continued, what
+! is the side length of the square spiral for which the ratio of primes along
+! both diagonals first falls below 10%?
+!
+! Project Euler: 58
+!
+! Answer: 26241!/
+
+
+program main
+
+use euler
+
+        implicit none
+
+        integer (int32), parameter :: END_PERCENT = 10; 
+        integer (int32) :: s1, s2, s3, diag_total, count, n;
+
+        n = 5;
+        diag_total = 5;
+        count = 3;
+
+        do while(.TRUE.)
+
+                s1 = (n * n) - (n - 1);
+                s2 = (n * n) - 2 * (n - 1);
+                s3 = (n * n) - 3 * (n - 1);
+
+                call inc(count, intbool(isprime(s1)) + intbool(isprime(s2)) &
+                        + intbool(isprime(s3)));
+                call inc(diag_total, 4);
+
+                if((100 * count) / diag_total < END_PERCENT) then
+                        exit
+                end if
+
+                call inc(n, 2);
+
+
+        end do
+
+        call printint(n);
+
+
+contains
+
+        function intbool (bool)
+
+                logical, intent(in) :: bool;
+                integer (int32) :: intbool;
+
+                intbool = MERGE(1, 0, bool);
+
+        end function intbool
+
+
+end program main
+

82f590e7930bdfda3f2e819fa428446f3b3a5218
Guy Nankivell <guy.nankivell@solnet.co.nz>
2018-07-05 16:42:01 +1200

    Because *NUMBER THEORY*

--- src/sum_sq_diff.c
+++ src/sum_sq_diff.c
@@ -1,4 +1,4 @@
-/* Find the difference between the 
+/* Find the difference between the
  * sum of the squares and the square
  * of the sum for all numbers up to 100
  *
@@ -16,18 +16,27 @@
 
 
 int main(int argc, char** argv);
+int64_t sum_consec_squares(int64_t);
+int64_t sum_consec_int(int64_t);
 
 
 int main(int argc, char** argv)
 {
-        uint64_t sqrsum = 0;
-        uint64_t total = 0;
+        (void) argc;
+        (void) argv;
 
-        for(uint64_t i = 0; i <= MAX; i++){
-                sqrsum += SQUARE(i);
-                total += i;
-        }
+        int64_t total = sum_consec_int(MAX);
 
-        printf("%"PRIu64"\n", SQUARE(total) - sqrsum);
+        printf("%"PRIu64"\n", SQUARE(total) - sum_consec_squares(MAX));
+}
+
+int64_t sum_consec_squares(int64_t n)
+{
+        return (n * (n + 1) * (2 * n + 1)) / 6;
+}
+
+int64_t sum_consec_int(int64_t n)
+{
+        return n * (n + 1) /2;
 }
 

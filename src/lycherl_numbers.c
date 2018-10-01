2beed4823329a483ef7dd32b41131d8d26b0d19c
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2018-10-02 12:32:04 +1300

    Far nicer implementation

--- src/lycherl_numbers.c
+++ src/lycherl_numbers.c
@@ -7,60 +7,58 @@
  * 4213 + 3124 = 7337
 
  * That is, 349 took three iterations to arrive at a palindrome.
- * 
- * Although no one has proved it yet, it is thought that some numbers, 
- * like 196, never produce a palindrome. A number that never 
+ *
+ * Although no one has proved it yet, it is thought that some numbers,
+ * like 196, never produce a palindrome. A number that never
  * forms a palindrome through the reverse and add process is called
- * a Lychrel number. Due to the theoretical nature of these numbers, 
- * and for the purpose of this problem, we shall assume that a number 
- * is Lychrel until proven otherwise. In addition you are given 
- * that for every number below ten-thousand, it will either (i) 
- * become a palindrome in less than fifty iterations, or, (ii) no one, 
- * with all the computing power that exists, has managed so far 
- * to map it to a palindrome. In fact, 10677 is the first 
- * number to be shown to require over fifty iterations 
- * before producing a palindrome: 
+ * a Lychrel number. Due to the theoretical nature of these numbers,
+ * and for the purpose of this problem, we shall assume that a number
+ * is Lychrel until proven otherwise. In addition you are given
+ * that for every number below ten-thousand, it will either (i)
+ * become a palindrome in less than fifty iterations, or, (ii) no one,
+ * with all the computing power that exists, has managed so far
+ * to map it to a palindrome. In fact, 10677 is the first
+ * number to be shown to require over fifty iterations
+ * before producing a palindrome:
  * 4668731596684224866951378664 (53 iterations, 28-digits).
- * 
- * Surprisingly, there are palindromic numbers that are 
+ *
+ * Surprisingly, there are palindromic numbers that are
  * themselves Lychrel numbers; the first example is 4994.
 
  * How many Lychrel numbers are there below ten-thousand?
 
- * Project Euler: 55 
+ * Project Euler: 55
  *
  * Answer: 249 */
 
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
-#include <math.h>
 
 
 #define MAX             10000
-#define LEN             100
-#define ITER_MAX        50 
+#define BASE            10
+#define ITER_MAX        50
 
 
-int isPalindrome(char *string);
-uint64_t stringInt(char *str);
-char *strCopy(char *string);
-int strLength(char *string);
-void strprint(char *str);
-char *strrev(char *str);
-int lycherl(int i);
-int main();
+uint64_t intrev(uint64_t i);
+int lycherl(uint64_t i);
+int is_palindrome(uint64_t i);
+int main(int argc, char** argv);
 
 
-int main()
+int main(int argc, char** argv)
 {
-        uint16_t count = 0;
+        (void) argc;
+        (void) argv;
 
-        for(uint16_t i = 0; i < MAX; count += lycherl(i++));
+        int32_t count = 0;
 
-        printf("%" PRIu16 "\n", count);
+        for(int32_t i = 0; i < MAX; count += lycherl(i++))
+                ;
+
+        printf("%"PRId32"\n", count);
 
         return 0;
 
@@ -69,100 +67,47 @@ int main()
 
 /* Returns whether the integer is lycherl or not
  * by checking up to ITER_MAX iterations */
-int lycherl(int i)
+int lycherl(uint64_t i)
 {
-          
-        uint16_t count = 0; uint64_t sum;
-        char *str = malloc(LEN * sizeof(char));
-        char *reverse;
 
-        sprintf(str, "%d", i);
+        int32_t count = 0;
 
-        if(isPalindrome(str)){
-                reverse = strrev(strCopy(str));
-                sum = stringInt(str) + stringInt(reverse);
-                sprintf(str, "%" PRIu64, sum);
+        if(is_palindrome(i)){
+                i += intrev(i);
+                count++;
         }
 
-        while(!isPalindrome(str)){
-                reverse = strrev(strCopy(str));
-                sum = stringInt(str) + stringInt(reverse);
+        while(!is_palindrome(i)){
 
-                if(count++ > ITER_MAX) return 1;
+                i += intrev(i);
 
-                sprintf(str, "%" PRIu64, sum);
+                if(count++ > ITER_MAX){
+                        return 1;
+                }
         }
 
         return 0;
 
-} 
-
-
-int isPalindrome(char *s)
-{
-	int l = strLength(s);
-
-	for(int i = 0; i < l/2; i++){
-		if ( s[i] != s[l-i-1] ) return 0; 
-	}
-
-	return 1;
 }
 
-
-int strLength(char *string)
+uint64_t intrev(uint64_t i)
 {
-	uint16_t i = 0;
+        uint64_t rev = 0;
+        uint64_t rem;
 
-	for( ; *string++; i++)
-                ;
-        
-        return i;
-}
-
-
-/* Simple string copy */
-char * strCopy(char *string)
-{
-        char *copy = malloc(30 * sizeof(char));
-
-        for(uint16_t i = 0 ; *string; copy[i++] = *string++);
-
-        return copy;
-
-}
-
-
-uint64_t stringInt(char *str)
-{
-        uint64_t dec = 0;
-
-        for( ; *str; dec = dec * 10 + (*str++ - '0'))
-                ;
-
-        return dec;
-}
-
-
-char *strrev(char *str)
-{
-        if (! str || ! *str) return str;
-
-        char *p1, *p2;
-                
-        /* Say what... */
-        for (p1 = str, p2 = str + strLength(str) - 1; p2 > p1; ++p1, --p2){
-                *p1 ^= *p2;
-                *p2 ^= *p1;
-                *p1 ^= *p2;
+        while(i){
+                rem = i % BASE;
+                rev *= BASE;
+                rev += rem;
+                i /= BASE;
         }
 
-        return str;
+        return rev;
 }
 
 
-void strprint(char *str)
+int is_palindrome(uint64_t i)
 {
-        for( ; *str; printf("%c", *str++));
-        printf("\n");
+        return (i == intrev(i));
 }
+

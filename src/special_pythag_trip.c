45b7289c0660a95f7dfe0d068e514c245b4c48e0
Guy Nankivell <guy.nankivell@solnet.co.nz>
2019-02-24 23:48:43 +1300

    Light refactor with performance enhancements

--- src/special_pythag_trip.c
+++ src/special_pythag_trip.c
@@ -1,26 +1,32 @@
-/* A Pythagorean triplet is a set of 
+/* A Pythagorean triplet is a set of
  * three natural numbers, a < b < c, for which,
  * a^2 + b^2 = c^2
  * For example, 32 + 42 = 9 + 16 = 25 = 52.
- * There exists exactly one Pythagorean triplet 
+ * There exists exactly one Pythagorean triplet
  * for which a + b + c = 1000.
  * Find the product abc.
- * 
+ *
  * Answer: 31875000
  * Project Euler: 9 */
 
 #include <stdio.h>
 #include <stdint.h>
-#include <math.h>
+#include <inttypes.h>
+
 
 #define PERIMETER       1000
 
+
 int main(int argc, char** argv);
-int ispythag(uint64_t a, uint64_t b, uint64_t c);
+int ispythag(uint32_t a, uint32_t b, uint32_t c);
+
 
 int main(int argc, char** argv)
 {
-        uint64_t a, b, c;
+        (void) argc;
+        (void) argv;
+
+        uint32_t a, b, c;
 
         for(a = 1; a < PERIMETER; a++){
 
@@ -37,14 +43,14 @@ int main(int argc, char** argv)
         }
 
 out:
-       
-        printf("%d\n", a * b * c); 
+
+        printf("%"PRIu32"\n", a * b * c);
 
         return 0;
 }
 
-int ispythag(uint64_t a, uint64_t b, uint64_t c)
+int ispythag(uint32_t a, uint32_t b, uint32_t c)
 {
-        return (pow(a, 2) + pow(b, 2) == pow(c, 2));
+        return ((a * a) + (b * b) == (c * c));
 }
 

ab25cf7a0dbce1e44034ca4737e55ee5cf5be0a0
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2019-04-12 19:45:53 +1200

    Large night ahead. Preparation is programming

--- src/largest_prime_factor.c
+++ src/largest_prime_factor.c
@@ -1,4 +1,4 @@
-/* Find largest prime factor of the
+/* Find pf prime factor of the
  * number 600851475143
  *
  * Answer: 6857
@@ -7,9 +7,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
-#include <math.h>
 
-int64_t MAX = 600851475143;
+#define MAX     600851475143
 
 int main(int argc, char** argv);
 int is_prime(int32_t s);
@@ -19,24 +18,22 @@ int main(int argc, char** argv)
         (void) argc;
         (void) argv;
 
-        int32_t largest = 0;
+        int32_t pf = 0;
 
-        int32_t limit = (int32_t) round(sqrt((double) MAX));
-
-        for(int32_t i = 1; i <= limit; i += ((largest = (MAX % i == 0 && is_prime(i)) ? i : largest), 1))
+        for(int32_t i = 1; ((int64_t) i * i) <= MAX;
+                i += ((pf = (!(MAX % i) && is_prime(i)) ? i : pf), 2))
                 ;
 
-        printf("%"PRId32"\n", largest);
+        printf("%"PRId32"\n", pf);
 }
 
 
 /* Detects whether int is prime */
 int is_prime(int32_t s)
 {
-        int32_t res = !(s <= 2 || s % 2 == 0);
-        int32_t top = (int32_t) round(sqrt((float) s));
+        int32_t res = !(s < 2 || !(s % 2));
 
-        for(int32_t i = 3; i <= top && res; i += ((res = !(s % i == 0)), 2))
+        for(int32_t i = 3; (i * i) <= s && res; i += (res = (s % i), 2))
                 ;
 
         return (res || s == 2);

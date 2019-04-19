a17701493d42aaccdfca4c6e586775d44d851117
Guy Nankivell <guy.nankivell@solnet.co.nz>
2019-04-19 15:26:58 +1200

    Theoretically cleaner

--- src/counting_rectangles.c
+++ src/counting_rectangles.c
@@ -1,16 +1,17 @@
 
+
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <stdlib.h>
 #include <inttypes.h>
+
 
 #define TARGET_N        2000000
 #define LIM             2000
-#define ABS(x)          (((x) < 0) ? (x) * -1 : (x))
+
 
 int main(int argc, char** argv);
 int32_t n_rectangles(int32_t h, int32_t v);
+
 
 int main(int argc, char** argv)
 {
@@ -20,19 +21,21 @@ int main(int argc, char** argv)
         int32_t nearest_area = 1;
         int32_t eps = TARGET_N;
 
-        for(int32_t i = 1; i < LIM; i+= 2){
-                for(int32_t j = 2; j < LIM; j+=2){
+        for(int32_t i = 1; i < LIM; i += 2){
+
+                for(int32_t j = 2; j < LIM; j += 2){
 
                         int32_t n_rect = n_rectangles(i, j);
-                        int32_t diff = ABS(n_rect - TARGET_N);
 
-                        if(n_rect > TARGET_N && diff > eps){
+                        if(n_rect > TARGET_N){
                                 break;
                         }
 
+                        int32_t diff = TARGET_N - n_rect;
+
                         if(diff < eps){
                                 eps = diff;
-                                nearest_area = i*j;
+                                nearest_area = i * j;
                         }
 
                 }

a7d439d35e21cb1aced7f8f1ae4b55f495cb7ae9
Guy Nankivell <guy.nankivell@solnet.co.nz>
2019-04-22 20:44:42 +1200

    Steamy commit

--- src/combinatoric_selections.c
+++ src/combinatoric_selections.c
@@ -26,8 +26,8 @@
 #include <inttypes.h>
 
 
+uint32_t count_combinations(uint32_t nmax, uint32_t maxval);
 uint64_t combination(uint16_t n, uint16_t r);
-uint32_t countCombinations(uint32_t nmax, uint32_t maxval);
 uint64_t factorial(uint16_t i, uint16_t n);
 int main(int argc, char** argv);
 
@@ -37,7 +37,7 @@ int main(int argc, char** argv)
         (void) argc;
         (void) argv;
 
-        printf("%"PRIu32"\n", countCombinations(NMAX, MAX_VAL));
+        printf("%"PRIu32"\n", count_combinations(NMAX, MAX_VAL));
 
         return 0;
 }
@@ -51,15 +51,15 @@ uint64_t combination(uint16_t n, uint16_t r)
 }
 
 
-/* In order to reduce the problem size I have tried to eliminate 
- * as many additional calculations as possible. This factorial function 
+/* In order to reduce the problem size I have tried to eliminate
+ * as many additional calculations as possible. This factorial function
  * also accepts an argument i; which then becomes the starting number for
- * the function, calculating the product up to n. This removes a few low 
+ * the function, calculating the product up to n. This removes a few low
  * hanging fruit. Displayed thusly with nCr with n = 9 and r = 2
  *
- *      1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 x 9        9! 
+ *      1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 x 9        9!
  *      ---------------------------------       ----
- *      1 x 2 x 1 x 2 x 3 x 4 x 5 x 6 x 7       2!7! 
+ *      1 x 2 x 1 x 2 x 3 x 4 x 5 x 6 x 7       2!7!
  *
  * Above we can see that we can eliminate the first 1 - 7 of both the
  * numerator and the denominator reducing this problem to:
@@ -68,7 +68,7 @@ uint64_t combination(uint16_t n, uint16_t r)
  *      -----
  *      1 x 2
  *
- * Which is a slightly smaller problem to calculate. We expect with the 
+ * Which is a slightly smaller problem to calculate. We expect with the
  * larger numbers to encounter the value bigger than MAX_VAL very early
  * on in the sequence thusly this optimises for when n - r is small */
 uint64_t factorial(uint16_t i, uint16_t n)
@@ -86,13 +86,13 @@ uint64_t factorial(uint16_t i, uint16_t n)
 /* This counts the "large" (values exceeding maxval) combinations in the form
  * of nCr where 1 <= n <= nmax and 1 <= r <= n. This however employs some
  * trickery to fit such large calculations into the space of 64 bit integers */
-uint32_t countCombinations(uint32_t nmax, uint32_t maxval)
+uint32_t count_combinations(uint32_t nmax, uint32_t maxval)
 {
         uint32_t count = 0;
 
-        /* We know that the combination function 
+        /* We know that the combination function
          * is symmetric and thusly once we have calculated
-         * one side of it, we can multiply the instances 
+         * one side of it, we can multiply the instances
          * that are bigger than 1mil by two to get the other side */
 
         for(uint16_t n = 1; n <= nmax; n++){
@@ -109,17 +109,11 @@ uint32_t countCombinations(uint32_t nmax, uint32_t maxval)
                         uint64_t k = combination(n, r);
 
                         if(k > maxval){
-
-                                if(n % 2 != 0) count++;
-
-                                count += (2 * (n/2 - r) + 1);
-
+                                count += !(n % 2) + (2 * (n/2 - r) + 1);
                                 break;
-
                         }
 
                 }
-
 
         }
 
